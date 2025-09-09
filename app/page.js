95629821cf6f62fdd557370a65268368f6c4beaf
mookil <mikael2aa@gmail.com>
2025-09-09 13:34:06 -0600

    added some styling
    - changed the github link to appear like the example (github: github link)

--- app/page.js
+++ app/page.js
@@ -5,11 +5,16 @@ export default function Page() {
   return (
     <main>
       <h1 className="text-3xl">CPRG 306: Web Development 2 - Assignments</h1>
-      <ul>
+      <hr></hr>
+
+      <p>Assignments</p>
+      <ul className="list-disc list-inside">
         <li>
           <Link href="./week-2" className="text-cyan-600 underline hover:text-cyan-300">Week 2</Link>
         </li>
       </ul>
+
+      
     </main>
   );
 }
--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -4,8 +4,9 @@ import Link from "next/link";
 export default function StudentInfo() {
     return (
         <div>
-            <p>Mikael Ly</p>
-            <Link href="https://github.com/mookil" className="text-cyan-600 underline hover:text-cyan-300">My Github Account</Link>
+            <p>Name: Mikael Ly</p>
+            <p>GitHub: <Link href="https://github.com/mookil" className="text-cyan-600 underline hover:text-cyan-300">github.com/mookil</Link></p>
+            
         </div>
     );
 }
