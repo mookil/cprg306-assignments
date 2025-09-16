99bcf93c206f418676635eecd17ddf9bcc839ed3
mookil <mikael2aa@gmail.com>
2025-09-16 13:48:59 -0600

    updated items.js
    changed parameters to use props

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,5 +1,7 @@
 
-export default function Item({ name, quantity, category}) {
+export default function Item(props) {
+
+    let {name, quantity, category} = props;
     return (
         <li className="border border-gray-300 rounded-md p-2 mb-2 ml-10 mr-10">
             {name} <br></br>
